b4d8d55f87f8b76167b861d49ce33d5f892e9eb3
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-05-12 19:46:11 +0200

    finished up the convenience cache loader

--- Biobase/GeneticCodes.hs
+++ Biobase/GeneticCodes.hs
@@ -8,12 +8,13 @@ module Biobase.GeneticCodes
   , translate
   ) where
 
+import Control.Lens
 import Control.Monad.Except
 import Data.List (find)
-import Text.Printf
-import Control.Lens
+import Data.Text (pack)
 import Data.Text (Text,isInfixOf,unpack)
 import System.Directory (doesFileExist)
+import Text.Printf
 
 import Biobase.GeneticCodes.Embedded
 import Biobase.GeneticCodes.Import
@@ -40,13 +41,23 @@ codeByTableNameInfix ts n
   = maybe (throwError $ printf "No TranslationTable with Name infix %s found!" $ unpack n) return
   $ find (\t → n `isInfixOf` (t^.tableName)) ts
 
+-- | If the given filepath exists, then we try to load the genetic table from
+-- the file. This will fail if there is not exactly one genetic table there. If
+-- @fp@ is not a file, we try parsing @fp@ as a numeric ID and look for that
+-- table. Finally we try finding an infix with that name.
+--
+-- This is all slightly "unsafe" but captures the most common scenario where we
+-- either load such a table from file or need selection of the correct one.
+
 fromFileOrCached
   ∷ (MonadIO m, MonadError String m)
   ⇒ FilePath
-  → m [TranslationTable Char Char]
+  → m (TranslationTable Char Char)
 fromFileOrCached fp = do
   dfe ← liftIO $ doesFileExist fp
-  if dfe
-    then fromFile fp
-    else pure geneticCodes
+  if | dfe → fromFile fp >>= \case
+         [x] → return x
+         xs  → throwError $ fp ++ " should contain exactly one translation table!"
+     | [(k,"")] ← reads fp → codeByTableID geneticCodes k
+     | otherwise → codeByTableNameInfix geneticCodes $ pack fp
 
