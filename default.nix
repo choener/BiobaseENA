a78dd8cb52ebda07255da12183121a2d84590054
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:41 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,19 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, base, bytestring, containers, directory, file-embed
+, lens, megaparsec, mtl, QuickCheck, stdenv, tasty
+, tasty-quickcheck, tasty-th, text, vector, vector-th-unbox
+}:
+mkDerivation {
+  pname = "BiobaseENA";
+  version = "0.0.0.1";
+  src = ./.;
+  libraryHaskellDepends = [
+    base bytestring containers directory file-embed lens megaparsec mtl
+    text vector vector-th-unbox
+  ];
+  testHaskellDepends = [
+    base QuickCheck tasty tasty-quickcheck tasty-th
+  ];
+  homepage = "https://github.com/choener/BiobaseENA";
+  description = "European Nucleotide Archive data";
+  license = stdenv.lib.licenses.bsd3;
+}
