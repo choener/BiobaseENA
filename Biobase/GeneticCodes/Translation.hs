366f386b21d11ff3c61be6ba8ca469381b9aba62
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-14 17:04:11 +0100

    more translation options added

--- Biobase/GeneticCodes/Translation.hs
+++ Biobase/GeneticCodes/Translation.hs
@@ -5,11 +5,16 @@
 --
 -- Limited "backtranslation" capabilities are provided. Since this process is
 -- lossy, it should only be used in very specific circumstances.
+--
+-- TODO Translation from @BioSequence RNA is missing.
 
 module Biobase.GeneticCodes.Translation where
 
 import           Control.Lens
 import qualified Data.Map.Strict as M
+import qualified Data.ByteString.Char8 as BS
+
+import           Biobase.Types.BioSequence
 
 import           Biobase.GeneticCodes.Types
 
@@ -35,4 +40,30 @@ instance Translation (BaseTriplet Char) where
   type AAType      (BaseTriplet Char) = Char
   translate tbl t = maybe 'X' _aminoAcid $ M.lookup t (tbl^.tripletToAminoAcid)
   {-# Inline translate #-}
+
+-- | Strings of characters are normally very inconvenient but useful in
+-- backtracking cases. Fully assumes that the alphabet is DNA. Ignores
+-- non-triplets at the end.
+
+instance Translation String where
+  type TargetType String = String
+  type TripletType String = Char
+  type AAType String = Char
+  translate tbl =
+    let go xs | [x,y,z] ← hd = translate tbl (BaseTriplet x y z) : go tl
+              | otherwise = []
+              where (hd,tl) = splitAt 3 xs
+    in  go
+
+-- | Translation of @BioSequence DNA@. The translation tables assume DNA
+-- triplets anyway. Biologically there should be a transcription step in
+-- between. Ignores non-triplets at the end.
+
+instance Translation (BioSequence DNA) where
+  type TargetType (BioSequence DNA) = BioSequence AA
+  type TripletType (BioSequence DNA) = Char
+  type AAType (BioSequence DNA) = Char
+  translate tbl (BioSequence xs) =
+    let go k = Just (translate tbl $ BaseTriplet (BS.index xs k) (BS.index xs (k+1)) (BS.index xs (k+2)) ,k+3)
+    in  BioSequence . fst $ BS.unfoldrN (BS.length xs `div` 3) go 0
 
